5d94e959bd8334b32f05f2137c214431d0168120
MujuniEli <elijahmujuni@gmail.com>
2024-03-07 21:34:47 +0300

    Added google maps

--- doc-profile-page/index.js
+++ doc-profile-page/index.js
@@ -1,3 +1,9 @@
+
+let map;
+
 function initMap() {
-    
+    map = new google.maps.Map(document.getElementsByClassName('map'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 10,
+    })
 }
