618c83b187f571f8c69e1bbab7cf8b87d5022002
MujuniEli <elijahmujuni@gmail.com>
2024-03-07 22:27:49 +0300

    maps fixes

--- doc-profile-page/index.js
+++ doc-profile-page/index.js
@@ -2,7 +2,7 @@
 let map;
 
 function initMap() {
-    map = new google.maps.Map(document.getElementsById('map'), {
+    map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: -34.397, lng: 150.644},
         zoom: 10,
     })
