d9f7357512fed6858ad3d1f4d8c0e860c1faac0e
MujuniEli <elijahmujuni@gmail.com>
2024-03-18 20:04:41 +0300

    Added styles and stopped map actions

--- doc-profile-page/index.js
+++ doc-profile-page/index.js
@@ -1,11 +1,11 @@
 
-let map;
+// let map;
 
-function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 0.41228, lng: 32.6438},
-        zoom: 10,
-    })
-}
+// function initMap() {
+//     map = new google.maps.Map(document.getElementById('map'), {
+//         center: {lat: 0.41228, lng: 32.6438},
+//         zoom: 13,
+//     })
+// }
 
 // 0.4122877915587625, 32.64386510452468
